2756281132f7bf6d2cc23d21fac94e11e5220789
superstar54 <xingwang1991@gmail.com>
2018-11-20 17:05:09 +0100

    add BAND to cp2k_example

--- xcp2k/cp2k_example.py
+++ xcp2k/cp2k_example.py
@@ -17,18 +17,17 @@ for i in [1, 4, 6, 7, 8, 9, 14, 15, 16]:
 #===============================================================================
 
 #===============================================================================
-calc_ot = CP2K()
+calc = CP2K()
 #===============================================================================
-CP2K_INPUT_OT = calc_ot.CP2K_INPUT  # This is the root of the input tree
-GLOBAL = CP2K_INPUT_OT.GLOBAL
-MOTION = CP2K_INPUT_OT.MOTION
-FORCE_EVAL = CP2K_INPUT_OT.FORCE_EVAL_add()
+CP2K_INPUT = calc.CP2K_INPUT  # This is the root of the input tree
+GLOBAL = CP2K_INPUT.GLOBAL
+MOTION = CP2K_INPUT.MOTION
+FORCE_EVAL = CP2K_INPUT.FORCE_EVAL_add()
 SUBSYS = FORCE_EVAL.SUBSYS
 DFT = FORCE_EVAL.DFT
 SCF = DFT.SCF
 #===============================================================================
 GLOBAL.Run_type = "GEO_OPT"  # energy_force, geo_opt, cell_opt
-# MOTION.GEO_OPT.Max_force = 1.0E-4
 
 FORCE_EVAL.Method = "Quickstep"
 FORCE_EVAL.Stress_tensor = 'ANALYTICAL'
@@ -47,7 +46,6 @@ DFT.MGRID.Rel_cutoff = 60
 
 DFT.XC.XC_FUNCTIONAL.Section_parameters = "PBE"
 
-
 DFT.QS.Method = 'GPW'
 DFT.QS.Eps_default = 1.0E-12
 DFT.QS.Map_consistent = True
@@ -60,15 +58,6 @@ SCF.Eps_scf = 1.0E-6
 SCF.Max_scf = 50
 SCF.OUTER_SCF.Eps_scf = 1.0E-6
 SCF.OUTER_SCF.Max_scf = 20
-
-SCF.OT.Algorithm = 'IRAC'
-SCF.OT.Ortho_irac = 'CHOL'
-SCF.OT.Eps_irac_switch = 1.0E-2
-SCF.OT.Minimizer = "CG"
-SCF.OT.N_diis = 7
-SCF.OT.Safe_diis = False
-SCF.OT.Preconditioner = 'FULL_ALL'
-SCF.OT.Energy_gap = 0.05
 
 
 for i in range(1, nele):
@@ -88,7 +77,17 @@ for i in range(1, nele):
 #DFT.PRINT.MO_CUBES.Nlumo = 30
 
 
-
+#===============================================================================
+CP2K_INPUT_OT = copy.deepcopy(CP2K_INPUT)
+SCF = CP2K_INPUT_OT.FORCE_EVAL_list[0].DFT.SCF
+SCF.OT.Algorithm = 'IRAC'
+SCF.OT.Ortho_irac = 'CHOL'
+SCF.OT.Eps_irac_switch = 1.0E-2
+SCF.OT.Minimizer = "CG"
+SCF.OT.N_diis = 7
+SCF.OT.Safe_diis = False
+SCF.OT.Preconditioner = 'FULL_ALL'
+SCF.OT.Energy_gap = 0.05
 #===============================================================================
 CP2K_INPUT_OT_VDW = copy.deepcopy(CP2K_INPUT_OT)
 DFT = CP2K_INPUT_OT_VDW.FORCE_EVAL_list[0].DFT
@@ -99,52 +98,30 @@ PAIR_POTENTIAL.Type = 'DFTD3'
 PAIR_POTENTIAL.Reference_functional = 'PBE'
 PAIR_POTENTIAL.R_cutoff = '[angstrom] 16'
 
-
+# print(CP2K_INPUT_OT.FORCE_EVAL_list[0].DFT.XC.VDW_POTENTIAL.Potential_type)
+# print(CP2K_INPUT_OT_VDW.FORCE_EVAL_list[0].DFT.XC.VDW_POTENTIAL.Potential_type)
 #===============================================================================
-calc_diag = CP2K()
+CP2K_INPUT_OT_NEB = copy.deepcopy(CP2K_INPUT_OT)
+DFT = CP2K_INPUT_OT_NEB.FORCE_EVAL_list[0].DFT
+CP2K_INPUT_OT_NEB.GLOBAL.Run_type = "BAND"
+BAND = CP2K_INPUT_OT_NEB.MOTION.BAND
+BAND.Nproc_rep = 36
+BAND.Band_type = 'CI-NEB'
+BAND.Number_of_replica = 12
+BAND.CONVERGENCE_CONTROL.Max_force = 0.0050
+BAND.CONVERGENCE_CONTROL.Rms_force = 0.0050
+# BAND.CONVERGENCE_CONTROL.Max_dr = 0.005
+# BAND.CONVERGENCE_CONTROL.Rms_dr = 0.005    
+BAND.Rotate_frames = 'F'
+BAND.Align_frames = 'F'
+BAND.CI_NEB.Nsteps_it = 20  
+OPTIMIZE_BAND = BAND.OPTIMIZE_BAND_add()
+OPTIMIZE_BAND.DIIS.Max_steps = 1000    
+# print(CP2K_INPUT_OT.MOTION.BAND.Band_type)
+# print(CP2K_INPUT_OT_NEB.MOTION.BAND.Band_type)
 #===============================================================================
-CP2K_INPUT_Diag = calc_diag.CP2K_INPUT  # This is the root of the input tree
-GLOBAL = CP2K_INPUT_Diag.GLOBAL
-MOTION = CP2K_INPUT_Diag.MOTION
-FORCE_EVAL = CP2K_INPUT_Diag.FORCE_EVAL_add()
-SUBSYS = FORCE_EVAL.SUBSYS
-DFT = FORCE_EVAL.DFT
-SCF = DFT.SCF
-#===============================================================================
-GLOBAL.Run_type = "GEO_OPT"  # energy_force, geo_opt, cell_opt
-# MOTION.GEO_OPT.Max_force = 1.0E-4
-
-FORCE_EVAL.Method = "Quickstep"
-FORCE_EVAL.Stress_tensor = 'ANALYTICAL'
-FORCE_EVAL.PRINT.FORCES.Section_parameters = "ON"
-FORCE_EVAL.PRINT.STRESS_TENSOR.Section_parameters = "ON"
-
-DFT.Basis_set_file_name = "BASIS_MOLOPT"
-DFT.Potential_file_name = "POTENTIAL"
-#DFT.Charge = 0
-#DFT.Multiplicity = 1
-#DFT.Uks = True
-
-DFT.MGRID.Ngrids = 5
-DFT.MGRID.Cutoff = 800
-DFT.MGRID.Rel_cutoff = 60
-
-DFT.XC.XC_FUNCTIONAL.Section_parameters = "PBE"
-
-
-DFT.QS.Method = 'GPW'
-DFT.QS.Eps_default = 1.0E-12
-DFT.QS.Map_consistent = True
-DFT.QS.Extrapolation = 'ASPC'
-DFT.QS.Extrapolation_order = 3
-
-SCF.Scf_guess = "ATOMIC"
-SCF.Eps_diis = 0.1
-SCF.Eps_scf = 1.0E-6
-SCF.Max_scf = 50
-SCF.OUTER_SCF.Eps_scf = 1.0E-6
-SCF.OUTER_SCF.Max_scf = 20
-
+CP2K_INPUT_DIAG = copy.deepcopy(CP2K_INPUT)
+SCF = CP2K_INPUT_DIAG.FORCE_EVAL_list[0].DFT.SCF
 SCF.Added_mos = 200
 SCF.SMEAR.Electronic_temperature = 500
 SCF.SMEAR.Method = 'FERMI_DIRAC'
@@ -154,21 +131,5 @@ SCF.MIXING.Alpha = 0.1
 SCF.MIXING.Beta = 1.5
 SCF.MIXING.Nbuffer = 8
 
-
-for i in range(1, nele):
-  ele = chemical_symbols[i]
-  KIND = SUBSYS.KIND_add('{0}'.format(ele))  # Section_parameters can be provided as argument.
-  KIND.Element = '{0}'.format(ele)
-  KIND.Basis_set = basis[ele]
-  KIND.Potential = 'GTH-PBE' 
-
-#DFT.PRINT.PDOS.Nlumo = 500
-#DFT.PRINT.PDOS.EACH.Qs_scf = 0
-#DFT.PRINT.PDOS.EACH.Geo_opt = 0
-#DFT.PRINT.PDOS.EACH.Md = 0
-#DFT.PRINT.PDOS.Add_last = 'NUMERIC'
-
-#DFT.PRINT.MO_CUBES.Nhomo = 30
-#DFT.PRINT.MO_CUBES.Nlumo = 30
-
-#===============================================================================
+# print(CP2K_INPUT_OT.FORCE_EVAL_list[0].DFT.SCF.MIXING.Method)
+# print(CP2K_INPUT_DIAG.FORCE_EVAL_list[0].DFT.SCF.MIXING.Method)
